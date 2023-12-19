f62187c339b4d655ae44f6949ddb3019ef6367ba
Khuongnguyen27 <nguyenhuukhuong27102000@gmail.com>
2023-12-19 13:12:32 +0700

    finish-feedbak

--- resources/views/admin/packages/pdf/invoice.blade.php
+++ resources/views/admin/packages/pdf/invoice.blade.php
@@ -10,7 +10,6 @@
 </head>
 
 <body>
-    <h1 class="text-center">History</h1>
     <div>
         <ul>
             <li>Member : {{ $package_user->user->name }}</li>
@@ -18,6 +17,8 @@
             <li>Expiration day : {{ $package_user->expiration_date }}</li>
         </ul>
     </div>
+    @if($package_user->package_id !== 4)
+    <h1 class="text-center">OutSide</h1>
     <table class="table table-bordered">
         <thead>
             <tr>
@@ -26,46 +27,71 @@
             </tr>
         </thead>
         <tbody>
-            @php
-            $total_balls = 0;
-            @endphp
+            @php $total_balls = 0; @endphp
             @foreach($items as $item)
+            @if($item->balls)
             <tr>
                 <td>{{ date('d-m-Y', strtotime($item->created_at)) }}</td>
                 <td>{{ $item->balls }}</td>
             </tr>
-            @php
-            $total_balls += $item->balls;
-            @endphp
+            @php $total_balls += $item->balls; @endphp
+            @endif
             @endforeach
         </tbody>
     </table>
-    <div>
-        <ul>
-            <li>Total Balls : {{ $total_balls }}</li>
-            @if($package_user->expiration_count)
-            <li>Expiration : {{ $package_user->expiration_count }}</li>
+    <ul>
+        <li>Total Balls: {{ $total_balls }}</li>
+    </ul>
+    @endif
+    @if($package_user->package_id === 4 || $package_user->package_id === 2)
+    <h1 class="text-center">InSide</h1>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>From</th>
+                <th>To</th>
+                <th>Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php $total_hours = 0; @endphp
+            @foreach($items as $item)
+            @if(isset($item->total_hour) && $item->total_hour > 0)
+            <tr>
+                <td>{{ date('d-m-Y', strtotime($item->created_at)) }}</td>
+                <td>{{ $item->hour_to }}</td>
+                <td>{{ $item->to_hour }}</td>
+                <td>{{ $item->total_hour }}</td>
+            </tr>
             @endif
-        </ul>
-        @if($package_user->expiration_count)
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>Expiration Date</th>
-                    <th>Description</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($package_user->expiration as $item)
-                <tr>
-                    <td>{{ date('d-m-Y', strtotime($item->expiration_date)) }}</td>
-                    <td>{{ $item->description }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        @endif
-    </div>
+            @php $total_hours += $item->total_hour; @endphp
+            @endforeach
+        </tbody>
+    </table>
+    <ul>
+        <li>Total Hours: {{ $total_hours }}</li>
+    </ul>
+    @endif
+    @if($package_user->expiration_count)
+    <h1 class="text-center">Expiration</h1>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Expiration Date</th>
+                <th>Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($package_user->expiration as $item)
+            <tr>
+                <td>{{ date('d-m-Y', strtotime($item->expiration_date)) }}</td>
+                <td>{{ $item->description }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    @endif
 </body>
 
 </html>
